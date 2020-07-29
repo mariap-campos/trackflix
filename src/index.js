9f758a9203716d325e64a0242eccf709e29ce6b9
Maria P. Campos <mpfc.maria@gmail.com>
2020-07-29 14:54:55 -0300

    roteamento final - aula 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 
 
@@ -14,6 +15,7 @@ ReactDOM.render(
   <Switch>
    < Route path="/" component={Home} exact />
    < Route path="/cadastro/video" component={CadastroVideo} />
+   < Route path="/cadastro/categoria" component={CadastroCategoria} />
    < Route component={Pagina404} /> {/* Página de erro caso o path nao exista */}
   </Switch>
 
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,17 +3,17 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroVideo(){
+function CadastroCategoria(){
     return (
         <PageDefault>
-            <h1>Cadastro de videos</h1>
+            <h1>Cadastro de Categorias</h1>
 
-            <Link  to="/cadastro/categoria">
-                Cadastrar Categoria
+            <Link  to="/">
+                Ir para home
             </Link>
         </PageDefault>
     )
 }
 
 
-export default CadastroVideo;
+export default CadastroCategoria;

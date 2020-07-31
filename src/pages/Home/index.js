3f524217ec742fd2a7be8c95b808893a1b1c529a
Maria P. Campos <mpfc.maria@gmail.com>
2020-07-31 14:57:53 -0300

    commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Menu from '../../components/Menu';
+import styled from 'styled-components';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
-import styled from 'styled-components';
+import Menu from '../../components/Menu';
 
 const AppWrapper = styled.div`
   background: var(--black);
@@ -13,41 +13,41 @@ const AppWrapper = styled.div`
 function Home() {
   return (
     <AppWrapper>
-    <Menu />
+      <Menu />
 
-    <BannerMain
-      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-      url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
-    />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+      />
 
-    <Carousel
-      ignoreFirstVideo
-      category={dadosIniciais.categorias[0]}
-    />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
 
-    <Carousel
-      category={dadosIniciais.categorias[1]}
-    />
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
 
-    <Carousel
-      category={dadosIniciais.categorias[2]}
-    />
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
 
-    <Carousel
-      category={dadosIniciais.categorias[3]}
-    />
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
 
-    <Carousel
-      category={dadosIniciais.categorias[4]}
-    />
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
 
-    <Carousel
-      category={dadosIniciais.categorias[5]}
-    />
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />
 
-    <Footer />
-  </AppWrapper>
+      <Footer />
+    </AppWrapper>
   );
 }
 
